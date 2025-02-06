13af52d0f4336e4d9b59cbabc1254bbef0b9d16b
sharmeen466 <sharmeennazeer2@gmail.com>
2025-02-06 22:49:08 +0500

    js exercise

---
+++ app.js
@@ -0,0 +1,215 @@
+// "MATH EXPRESSION"
+
+//....................................chap # 5  START.........................................................
+
+
+// Q   #   01..
+
+// let a = 3;
+// let b = 5;
+// document.write("Sum of ", a, "and ", b ," = ", a + b );
+
+// Q   #   02..
+
+// let a = 3;
+// let b = 5;
+// document.write("a - b = ",a - b ,"<br>");
+// document.write("a * b = ",a * b ,"<br>");
+// document.write("a / b = ",a / b ,"<br>");
+// document.write("a % b = ",a % b ,"<br>");
+
+// Q   #    03..
+
+// A:
+// let myName; (Declearation)
+
+// B:
+// document.write("“Value after variable declaration is: Initialization”.")
+
+// C:
+// let a = 5
+
+// D:
+// document.write("Initial value: 5 ");
+
+// E:
+// console.log(++a);
+
+// F:
+// document.write("“Value after increment is: 6”.");
+
+// G:
+// console.log(a += 7);
+
+// H:
+// document.write("“Value after addition is: 13”.");
+
+// I:
+// console.log(--a);
+
+// J:
+// document.write("“Value after decrement is: 12”.")
+
+// K:
+// console.log( a % 3);
+
+// L:
+// document.write("The remainder is : 0.")
+
+// Q   #    04..
+
+// let costOfTicket = 600;
+// let buyingTicket = 5
+// document.write("Total cost to buy 5 tickets to a movie is ", costOfTicket * buyingTicket)
+
+// Q   #    05..
+
+// Q   #    06..
+
+// let calsius = 25
+// let Fahrenheit = 70
+// document.write("C = ( F -  32 ) * 5 / 9 <br>") 
+// document.write("C =(", Fahrenheit , " - 32 ) * 5 / 9 <br>") 
+// document.write("C = 38 * 5 / 9 <br>") 
+// document.write("C = 21.11111111111111")
+
+// document.write("<br> <br> <br>") 
+
+// document.write("F = ( C * 9 / 5 ) + ۤ32 <br>") 
+// document.write("F =(", calsius , " * 9 /5 ) + 32 <br>") 
+// document.write("F = (45 * 5 / 9) + 32 <br>") 
+// document.write("F = 45 + 32 <br>") 
+// document.write("F = 77 ") 
+
+// document.write("<br> <br> <br>") 
+
+// document.write("25C is 77F <br>") 
+// document.write("70F is 21.11111111111111") 
+
+
+// Q   #    07..
+
+// let item1 = 650
+// let item2 = 100
+// let quantityOfItem1 = 3
+// let quantityOfItem2 = 7
+// let doc = 100
+// document.write("Price of item 1 is ", item1, "<br>");
+// document.write("Quantity of item 1 is ", quantityOfItem1, "<br>");
+// document.write("Price of item 2 is ", item2, "<br>");
+// document.write("Quantity of item 2 is ", quantityOfItem2, "<br>");
+// document.write("Shipping charges ", doc, "<br>");
+// document.write("Total cost of your order is ", item1 * quantityOfItem1 + item2 * quantityOfItem2 + doc);  
+
+// Q   #    08..
+
+// let totalMarks = 980
+// let marksObtained = 804
+
+// document.write("Total Marks: ", totalMarks, "<br>")
+// document.write("Marks Obtained: ", marksObtained, "<br>")
+// document.write("Percentage: 82.04081632653061%")
+
+// Q   #    09..
+
+// let perDollar = 104.80;
+// let perRiyal = 28;
+// document.write("<h1>Currency in PKR </h1>", "</br>")
+// document.write("Total currency in PKR: ", perDollar * 10 + perRiyal * 25)
+
+// Q   #    10..
+
+// let number = 10;
+// console.log("10 + 5 = ", number + 5);
+// console.log("10 * 10 = ", number * 10);
+// console.log("10 / 2 = ", number / 2);
+
+// Q   #    11..
+
+// let crntYear = 2025;
+// let birthYear = 2000;
+// document.write ("<h1> Age Calculator </h1>", "<br>")
+// document.write ("Current Year: ", crntYear, "</br>")
+// document.write ("Birth Year: ", birthYear, "</br>")
+// document.write ("Your Age is: ", crntYear - birthYear)
+
+// Q   #    12..
+
+// let radius = 20;
+// let circumference = 125.67999999999;
+// document.write("<h1> The Geometrizer </h1>", "</br>")
+// document.write("Radius of a circle is: ", radius, "</br>")
+// document.write("The circumference is: ", circumference, "</br>")
+// document.write("The area is: 1256.8")
+
+// Q   #    13..
+
+// let snacke = "Chocolate Cake";
+// let age = 20;
+// let maxAge = 50;
+// let amountPerDay = 2;
+// let eat = (50 - 20 )* 2;
+// document.write("<h1>The Lifetime Supply Calculator </h1>", "</br>")
+// document.write("Favourite Snack: ", snacke, "</br>")
+// document.write("Current Age: ", age, "</br>")
+// document.write("Estimated Maximun Age: ", maxAge, "</br>")
+// document.write("Amount of Snackes per day: ", amountPerDay, "</br>")
+// document.write("You will need ", eat , " Chocolate Cakes to last yu untilthe ripe old age of 50")
+
+// Q   #    14..
+
+//...................................chap # 5   END.........................................................
+
+
+
+//....................................chap # 6 to 9  START.........................................................
+
+//Q   #    1..
+
+// let a = 10;
+
+// document.write ("The value of a is: ",a, "</br>")
+// document.write ("..............................................", "</br></br>")
+// document.write ("The value of ++a is: ", ++a, "</br>")
+// document.write ("Noe the value of a is: ", a, "</br></br>")
+// document.write ("The value of a++ is:", a++, "</br>")
+// document.write ("Now the value of a++ is: ", a, "</br></br>")
+// document.write ("The value of --a is: ", --a, "</br>")
+// document.write ("Now the value of --a is:", a, "</br></br>")
+// document.write ("The value of a-- is ", a--, "</br>")
+// document.write ("Now the value of a-- is ", a, "</br>")
+
+//Q   #    2..
+
+// let a = 2;
+// let b = 1;
+// document.write("a = 2","</br>")
+// document.write("b = 1","</br>")
+// document.write("Find = --a  -  --b  +  ++b  +  b--  ","</br>")
+// document.write("Result = 3")
+
+//Q   #    3..
+
+// let user = prompt('Enter your name')
+// alert ("Hello "+ user)
+
+//Q   #    5..
+
+//Q   #    6..
+
+// let sub1 = "English"
+// let sub2 = "Urdu"
+// let sub3 = "Math"
+// let totalMarks = 100
+// let sub1Marks = 60
+// let sub2Marks = 80
+// let sub3Marks = 50
+
+// document.write("<table> <tr> <th> Subject </th> <th> Total Marks </th>  <th> Obtained Marks</th> <th> Percentage </th> </td>"+ "<br>")
+// document.write("<tr><td>", sub1, "</td> <td>", totalMarks, "</td> <td>", sub1Marks, "</td> <td> 60% </td> </tr> <br>")
+// document.write("<tr><td>", sub2, "</td> <td>", totalMarks, "</td> <td>", sub2Marks, "</td> <td> 80% </td> </tr> <br>")
+// document.write("<tr><td>", sub3, "</td> <td>", totalMarks, "</td> <td>", sub3Marks, "</td> <td> 50% </td> </tr> <br>")
+// document.write("<tr><td>", "</td> <td> <strong>", totalMarks * 3, "</strong></td> <td><strong>", sub1Marks + sub2Marks + sub3Marks, "</strong></td> <td><strong> 80% </strong></td>  </tr> </table>")
+
+
+//....................................chap # 6 to 9  END.........................................................
